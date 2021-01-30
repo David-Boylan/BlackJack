3a8ce29a26891ac5978b0c603f15faa504cb34c4
David Boylan <drboylan734@gmail.com>
2021-01-30 16:06:00 -0600

    added com still does not work

--- blackjackGame/Blackjack.java
+++ blackjackGame/Blackjack.java
@@ -11,9 +11,11 @@ public class Blackjack {
         boolean pickedACard = false;
         while (reply == "yes" || reply == "Yes" || reply == "y" || reply == "Y") {
             Dealer.shuffle();
-            boolean win = true;
+            boolean win = false;
             int i = 0;
-            int cardTotal = 0;
+            int c = 0;
+            int playerCardTotal = 0;
+            int comCardTotal = 0;
             String hitStand = "hit";
             String playerCard1 = "";
             String playerCard2 = "";
@@ -29,19 +31,33 @@ public class Blackjack {
                 playerCard1 = deal.DealPlayer();
                 if (playerCard1 != "nothing") {
                     i++;
-                    cardTotal = card.value(playerCard1);
+                    playerCardTotal = card.value(playerCard1);
+                }
+            }
+            while (c < 1) { // get card 1
+                comCard1 = deal.DealPlayer();
+                if (comCard1 != "nothing") {
+                    c++;
+                    comCardTotal = card.value(comCard1);
                 }
             }
             while (i < 2) { // get card 2
                 playerCard2 = deal.DealPlayer();
                 if (playerCard2 != "nothing") {
                     i++;
-                    cardTotal += card.value(playerCard2);
+                    playerCardTotal += card.value(playerCard2);
                 }
             }
-            while (hitStand.equals("hit") && playerCard5.equals("")) {
+            while (i < 2) { // get card 2
+                comCard2 = deal.DealPlayer();
+                if (comCard2 != "nothing") {
+                    c++;
+                    comCardTotal += card.value(comCard2);
+                }
+            }
+            while (hitStand.equals("hit") && playerCard5.equals("") && playerCardTotal < 22) {
                 pickedACard = false;
-                System.out.println("your total card total is " + cardTotal + " would you like to hit or stand");
+                System.out.println("your total card total is " + playerCardTotal + " would you like to hit or stand");
                 hitStand = Gargrave.next();
                 if (hitStand.equals("hit") || hitStand.equals("Hit") || hitStand.equals("h") || hitStand.equals("H")) {
                     if (playerCard3 == "" && pickedACard == false) {
@@ -49,7 +65,10 @@ public class Blackjack {
                             playerCard3 = deal.DealPlayer();
                             if (playerCard3 != "nothing" && pickedACard == false) {
                                 i++;
-                                cardTotal = cardTotal + card.value(playerCard3);
+                                playerCardTotal = playerCardTotal + card.value(playerCard3);
+                                if (playerCardTotal > 21) {
+
+                                }
                                 pickedACard = true;
                             }
                         }
@@ -59,7 +78,7 @@ public class Blackjack {
                             playerCard4 = deal.DealPlayer();
                             if (playerCard4 != "nothing") {
                                 i++;
-                                cardTotal = cardTotal + card.value(playerCard4);
+                                playerCardTotal = playerCardTotal + card.value(playerCard4);
                                 pickedACard = true;
                             }
                         }
@@ -69,12 +88,56 @@ public class Blackjack {
                             playerCard5 = deal.DealPlayer();
                             if (playerCard5 != "nothing") {
                                 i++;
-                                cardTotal = cardTotal + card.value(playerCard5);
+                                playerCardTotal = playerCardTotal + card.value(playerCard5);
                                 pickedACard = true;
                             }
                         }
                     }
                 }
+            }
+            while (comCardTotal < 16) {
+                pickedACard = false;
+                if (comCard3 == "" && pickedACard == false) {
+                    while (c < 3) {
+                        comCard3 = deal.DealPlayer();
+                        if (comCard3 != "nothing" && pickedACard == false) {
+                            c++;
+                            comCardTotal = comCardTotal + card.value(comCard3);
+                            if (comCardTotal > 21) {
+
+                            }
+                            pickedACard = true;
+                        }
+                    }
+                }
+                if (comCard4 == "" && pickedACard == false) {
+                    while (c < 4) {
+                        comCard4 = deal.DealPlayer();
+                        if (comCard4 != "nothing") {
+                            c++;
+                            comCardTotal = comCardTotal + card.value(comCard4);
+                            pickedACard = true;
+                        }
+                    }
+                }
+                if (comCard5 == "" && pickedACard == false) {
+                    while (c < 5) {
+                        comCard5 = deal.DealPlayer();
+                        if (comCard5 != "nothing") {
+                            c++;
+                            comCardTotal = comCardTotal + card.value(comCard5);
+                            pickedACard = true;
+                        }
+                    }
+                }
+            }
+            if (playerCardTotal > comCardTotal) {
+                win = true;
+            }
+            if (win == true) {
+                System.out.println("You Win :)");
+            } else {
+                System.out.println("computer wins :(");
             }
         }
     }
