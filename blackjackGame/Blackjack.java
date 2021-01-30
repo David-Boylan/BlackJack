44c9b09816515643e6925f2ef800efa988ece349
David Boylan <drboylan734@gmail.com>
2021-01-30 13:53:26 -0600

    fixed problems

--- blackjackGame/Blackjack.java
+++ blackjackGame/Blackjack.java
@@ -1,4 +1,5 @@
 package blackjackGame;
+
 import java.util.*;
 
 public class Blackjack {
@@ -8,11 +9,10 @@ public class Blackjack {
         Cards card = new Cards();
         String reply = "yes";
         boolean pickedACard = false;
-        while (reply == "yes" || reply == "Yes" || reply == "y"|| reply == "Y"){
-            System.out.println("the code has restarted");
+        while (reply == "yes" || reply == "Yes" || reply == "y" || reply == "Y") {
             Dealer.shuffle();
             boolean win = true;
-            int i=0;
+            int i = 0;
             int cardTotal = 0;
             String hitStand = "hit";
             String playerCard1 = "";
@@ -25,49 +25,50 @@ public class Blackjack {
             String comCard3 = "";
             String comCard4 = "";
             String comCard5 = "";
-            while (i<1){ //get card 1
+            while (i < 1) { // get card 1
                 playerCard1 = deal.DealPlayer();
-                if (playerCard1 != "nothing"){
+                if (playerCard1 != "nothing") {
                     i++;
                     cardTotal = card.value(playerCard1);
                 }
             }
-            while (i<2){ //get card 2
+            while (i < 2) { // get card 2
                 playerCard2 = deal.DealPlayer();
-                if (playerCard2 != "nothing"){
+                if (playerCard2 != "nothing") {
                     i++;
                     cardTotal += card.value(playerCard2);
                 }
             }
-            while (hitStand == "hit" && playerCard5 == "" && win != false){
+            while (hitStand.equals("hit") && playerCard5.equals("")) {
                 pickedACard = false;
                 System.out.println("your total card total is " + cardTotal + " would you like to hit or stand");
                 hitStand = Gargrave.next();
-                if (hitStand == "hit" || hitStand == "Hit" || hitStand == "h" || hitStand == "H"){
-                    if (playerCard3 == "" && pickedACard == false && hitStand != "stand"){
-                        while(i<3){
+                if (hitStand.equals("hit") || hitStand.equals("Hit") || hitStand.equals("h") || hitStand.equals("H")) {
+                    if (playerCard3 == "" && pickedACard == false) {
+                        while (i < 3) {
                             playerCard3 = deal.DealPlayer();
-                            if(playerCard3 != "nothing" && pickedACard == false && hitStand != "stand"){
+                            if (playerCard3 != "nothing" && pickedACard == false) {
                                 i++;
                                 cardTotal = cardTotal + card.value(playerCard3);
                                 pickedACard = true;
                             }
                         }
                     }
-                    if (playerCard4 == "" && pickedACard == false && hitStand != "stand"){
-                        while(i<4){
+                    if (playerCard4 == "" && pickedACard == false) {
+                        while (i < 4) {
                             playerCard4 = deal.DealPlayer();
-                            if(playerCard4 != "nothing"){
+                            if (playerCard4 != "nothing") {
                                 i++;
                                 cardTotal = cardTotal + card.value(playerCard4);
                                 pickedACard = true;
                             }
                         }
                     }
-                    if (playerCard5 == "" && pickedACard == false && hitStand != "stand"){
-                        while(i<5){
+                    if (playerCard5 == "" && pickedACard == false) {
+                        System.out.println("helo");
+                        while (i < 5) {
                             playerCard5 = deal.DealPlayer();
-                            if(playerCard5 != "nothing"){
+                            if (playerCard5 != "nothing") {
                                 i++;
                                 cardTotal = cardTotal + card.value(playerCard5);
                                 pickedACard = true;
